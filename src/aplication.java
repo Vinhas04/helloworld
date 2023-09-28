7f813c5809658ba62d63a73a48e82b7abe4c83d4
Vinhas04 <tomasvinhas@gmail.com>
2023-09-28 19:55:41 +0100

    Added the version number

--- src/aplication.java
+++ src/aplication.java
@@ -1,6 +1,7 @@
-public class aplication {
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Version 1.0");
     }
 }
 
